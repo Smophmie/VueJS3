7be44f6c2adf1d6951f4620717c5b457704e8de3
Sophie Théréau <sophiethereau@hotmail.fr>
2024-04-26 11:06:35 +0200

    utilisation d'une variable

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <HelloWorld/>
+  <HelloWorld :sub-title="subTitle"/>
 </template>
 
 <script setup>
   import HelloWorld from "./components/HelloWorld.vue";
+
+  const subTitle = "Lorem ipsum dolor sit amet consectetur adipisicing elit.";
 </script>
 
 <style scoped>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,11 @@
 <template>
   <h1>Hello world!</h1>
+  <p>{{ props.subTitle }}</p>
 </template>
 
 <script setup>
+
+  const props = defineProps(["subTitle"]);
 
 </script>
 
