bc70d93086eedd31910c406b2251da36d400ca6e
Sophie Théréau <sophiethereau@hotmail.fr>
2024-04-26 11:30:31 +0200

    utilisation d'une condition

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
 
+  <!-- On affiche le template HelloWorld et on lui passe la variable user -->
   <HelloWorld :user="user"/>
+
+  <!-- Affichage de la div à condition que l'âge saisi soit supérieur à 15 -->
+  <div
+    v-if="user.age>15"
+    >
+    Mon âge
+  </div>
+
   <input type="text" v-model="user.name">
   <input type="text" v-model="user.age">
 
@@ -8,15 +17,22 @@
 
 <script setup>
 
+  // On importe le template
   import HelloWorld from "./components/HelloWorld.vue";
 
-  import {ref} from "vue";
+  import {reactive} from "vue";
 
-  const user = ref({
+  // On crée un objet
+  const user = reactive({
     name: "Sophie",
     age: 28,
   });
 
+  const display = () => {
+    console.log(user);
+  }
+
+  display();
 
 </script>
 
